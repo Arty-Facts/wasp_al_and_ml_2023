67751f3015d0b46b7ca23ce49f0e74d42d3f5c69
Arturas Aleksandraus <Arturas.Aleksandraus@contextvision.se>
2023-04-14 16:06:35 +0200

    winner

--- exercise1.pl
+++ exercise1.pl
@@ -1,5 +1,5 @@
 %%%% The Problog program implementing the Bayesian network.
-% Cheater -> Draw1, Cheater -> Draw2, Cheater -> Coin,
+% Cheater -> Draw2, Cheater -> Coin,
 % Draw1 -> Highest
 % Draw2 -> Highest
 % Highest -> Coin
@@ -9,140 +9,65 @@
 %%%%%%%%%
 % Facts %
 %%%%%%%%%
+% default probs
+card(1,jack).
+card(2,queen).
+card(3,king).
+card(4,ace).
+
 heads.
 tails.
 
-%%%%%%%%%%%%%%%%%%%%%%%
-% Probabilistic Facts %
-%%%%%%%%%%%%%%%%%%%%%%%
 1/5::cheater.
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Probabilistic Fact Disjunctions %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% default probs
-1/4::jack;
-1/4::queen; 
-1/4::king;
-1/4::ace.
 
 %%%%%%%%%%%
 % Clauses %
 %%%%%%%%%%%
 % player1 is not affected by if player2 cheats use default probs
-draw1(X) :- X.
-% if player2 is fair use default probs  
-draw2(X) :- X, \+ cheater.
+1/4::draw1(X) :- card(_, X).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%
-% Probabilistic Clauses %
-%%%%%%%%%%%%%%%%%%%%%%%%%
+% if player2 is fair use default probs  
+1/4::draw2(X) :- card(_, X), \+ cheater.
 % if player2 cheats replaced all the jack cards with ace 
-0::draw2(jack) :- cheater.
-1/2::draw2(ace) :- cheater.
+0::draw2(jack) :- card(1, jack), cheater.
+1/2::draw2(ace) :- card(4, ace), cheater.
+1/4::draw2(X) :- card(V, X) , V>1, V<4 ,cheater.
+
+
+% coin is unfair if there is a cheater
+0::coin(heads); 1::coin(tails) :- cheater.
 
 % coin is fair if nobody cheats
 1/2::coin(_) :- \+ cheater.
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Probabilistic Clause Disjunctions %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% coin is unfair if there is a cheater
-0::coin(heads); 1::coin(tails) :- cheater.
+same(D1, D2) :- card(V1, D1), draw1(D1), card(V2, D2), draw2(D2),  V1=V2.
+highest1(D1, D2) :- card(V1, D1), draw1(D1), card(V2, D2), draw2(D2),  V1>V2.
+highest2(D1, D2) :- card(V1, D1), draw1(D1), card(V2, D2), draw2(D2),  V1<V2.
 
-%%%%%%%%%%
-% Querys %
-%%%%%%%%%%
+winner(D1, D2) :- highest1(D1, D2), \+ highest2(D1, D2).
+winner(D1, D2) :- same(D1, D2), coin(heads).
 
-evidence(cheater, true).
+winner :- highest1(D1, D2), \+ highest2(D1, D2).
+winner :- same(D1, D2), coin(heads).
 
-query(draw1(jack)).
-query(draw2(jack)).
-query(draw2(ace)).
-query(coin(heads)).
+evidence(cheater, false).
+%evidence(draw1(jack)).
+%evidence(draw2(king)).
+
+%query(highest1(_, _)).
+%query(highest2(_, _)).
+%query(same(_, _)).
+%query(coin(heads)).
+query(winner).
 
 
 %%%% Is Draw1 marginally independent of Coin ? ( Yes / No)    
-%%%% Your answer : 
+%%%% Your answer : Yes. Since Draw1 is not a descendent of Coin, Draw1 dose not influence Coin toss and vice versa.
 
 %%%% Is Draw1 marginally independent of Coin given Winner ? ( Yes / No)    
-%%%% Your answer : 
+%%%% Your answer : No since Winner acts as a collider.
 
 %%%% Given the observations in Table 1, learn the probability that player 2 is a cheater (keep the other parameters fixed). Use the learning tab from the online editor to do this. What is the final probability?
 %%%% Your answer : 
 
-
-
-% TEST
-%%%%%%%%%
-% Facts %
-%%%%%%%%%
-heads.
-tails.
-card(1,jack). 
-card(2,queen). 
-card(3,king).
-card(4,ace).
-
-%%%%%%%%%%%%%%%%%%%%%%%
-% Probabilistic Facts %
-%%%%%%%%%%%%%%%%%%%%%%%
-1/5::cheater.
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Probabilistic Fact Disjunctions %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% default probs
-1/4::jack;
-1/4::queen; 
-1/4::king;
-1/4::ace.
-
-%%%%%%%%%%%
-% Clauses %
-%%%%%%%%%%%
-% player1 is not affected by if player2 cheats use default probs
-draw1(X) :- X.
-% if player2 is fair use default probs  
-draw2(X) :- X, \+ cheater.
-
-%%%%%%%%%%%%%%%%%%%%%%%%%
-% Probabilistic Clauses %
-%%%%%%%%%%%%%%%%%%%%%%%%%
-% if player2 cheats replaced all the jack cards with ace 
-0::draw2(jack) :- cheater.
-1/2::draw2(ace) :- cheater.
-
-% coin is fair if nobody cheats
-1/2::coin(_) :- \+ cheater.
-values(v1, v2) :- card(v1, d1), card(v2, d2).
-%highest(h) :- 
-%    draw1(d1), 
-%    draw2(d2), 
-%    card(v1, d1), 
-%    card(v2, d2),
-%    v1 < v2, 
-%    h is d2.
-    
-%highest(h) :- draw1(d1), draw2(d2), card(v1, d1), card(v2, d2), v1 > v2, h is d2.
-
-%highest(h) :- draw1(d1), draw2(d2), card(v1, d1), card(v2, d2), v1 = v2, coin(tails), h is d2.
-%highest(h) :- draw1(d1), draw2(d2), card(v1, d1), card(v2, d2), v1 = v2, coin(heads), h is d1.
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Probabilistic Clause Disjunctions %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% coin is unfair if there is a cheater
-0::coin(heads); 1::coin(tails) :- cheater.
-
-%%%%%%%%%%
-% Querys %
-%%%%%%%%%%
-
-evidence(cheater, true).
-
-%query(draw1(jack)).
-%query(draw2(jack)).
-%query(draw2(ace)).
-%query(coin(heads)).
-query(values(_, _)).

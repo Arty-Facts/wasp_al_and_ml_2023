8be011ea2ff5a7bfc9effe8d48396333480dcbe5
Arturas Aleksandraus <Arturas.Aleksandraus@contextvision.se>
2023-04-17 17:06:32 +0200

    same done

--- exercise1.pl
+++ exercise1.pl
@@ -10,6 +10,11 @@
 % Facts %
 %%%%%%%%%
 % there are cards with values 
+1/4::suit(jack);
+1/4::suit(queen);
+1/4::suit(king);
+1/4::suit(ace).
+
 card(1,jack).
 card(2,queen).
 card(3,king).
@@ -28,22 +33,18 @@ tails.
 %%%%%%%%%%%
 % player1 is not affected by if player2 cheats use default probs
 % There exists a draw action that can take a card of any value with 1/4 probability
-1/4::draw1(X) :- card(_, X).
+draw1(X) :- suit(X).
 
 % if player2 is fair use default probs 
 % There exists a draw action that can take a card of any value with 1/4 probability if there is no cheater
-1/4::draw2(X) :- card(_, X), \+ cheater.
+draw2(X) :- suit(X), \+ cheater.
 
 % if player2 cheats replaced all the jack cards with ace 
-% There exists a draw action that can take a card of jack with value 1 that will never happen if there is a cheater
-0::draw2(jack) :- card(1, jack), cheater.
-% There exists a draw action that can take a card of ace with value 4 that will have 1/2 probability if there is a cheater
-1/2::draw2(ace) :- card(4, ace), cheater.
+% There exists a draw action that can take a card of ace with probability of jack
+draw2(ace) :- suit(jack), cheater.
 
-% There exists a draw action that can take a card of any value  that is grater then 1 and less 
-% then 4 with 1/2 probability if there is a cheater
-1/4::draw2(X) :- card(V, X) , V>1, V<4 ,cheater.
-
+% There exists a draw action that can take a card of any value that is grater then one
+draw2(X) :- suit(X), card(V, X) , V>1, cheater.
 
 % coin is unfair if there is a cheater
 0::coin(heads); 1::coin(tails) :- cheater.
@@ -52,11 +53,15 @@ tails.
 1/2::coin(_) :- \+ cheater.
 
 % There exists a Same state for two card suits that have the same value and player1 and player2 have drawn thea's card.
-same(D1, D2) :- card(V1, D1), draw1(D1), card(V2, D2), draw2(D2),  V1=V2.
-% There exists a highest1 state for two card suits where value V1 is higher then V2 and player1 and player2 have drawn thea's card.
-highest1(D1, D2) :- card(V1, D1), draw1(D1), card(V2, D2), draw2(D2),  V1>V2.
-% There exists a highest2 state for two card suits where value V2 is higher then V1 and player1 and player2 have drawn thea's card.
-highest2(D1, D2) :- card(V1, D1), draw1(D1), card(V2, D2), draw2(D2),  V1<V2.
+same(draw1, draw2) :- draw1(D), draw2(D).
+
+% There exists a highest1 state for two draws where the card value V1 is higher 
+% then V2 and player1 and player2 have drawn thea's card.
+highest1(draw1, draw2) :- card(V1, D1), draw1(D1), card(V2, D2), draw2(D2),  V1>V2.
+
+% There exists a highest2 state for two draws where the card value V2 is higher 
+% then V1 and player1 and player2 have drawn thea's card.
+highest2(draw1, draw2) :- card(V1, D1), draw1(D1), card(V2, D2), draw2(D2),  V1<V2.
 
 % There exists a state where player 1 has the highest value but not player2 
 winner(D1, D2) :- highest1(D1, D2), \+ highest2(D1, D2).
@@ -64,13 +69,13 @@ winner(D1, D2) :- highest1(D1, D2), \+ highest2(D1, D2).
 % There exists a state where player 1 and player 2 has the same card but not the coin is heads 
 winner(D1, D2) :- same(D1, D2), coin(heads).
 
-evidence(winner(jack, _), false).
-evidence(winner(ace, _), true).
-evidence(winner(ace, _), false).
-evidence(winner(king, _), false).
-evidence(winner(queen, _), false).
+evidence(cheater, false).
+%evidence(draw1(king), true).
 
-query(cheater).
+query(highest1(_,_)).
+ 
+
+%query(draw2(_)).
 
 
 %%%% Is Draw1 marginally independent of Coin ? ( Yes / No)    
